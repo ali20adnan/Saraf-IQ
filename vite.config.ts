d5208382b8b0b19780ebe8b09703a77ec0af3474
microgiftsx-sudo <microgiftsx@gmail.com>
2026-04-04 10:43:10 -0700

    سيبسيبسيب

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
@@ -6,7 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss() as any],
+      },
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
